8039cb30b8717ecf0abec4cf0ff713c76b0e5594
Alex Hewson <112609926+Alexandrite22@users.noreply.github.com>
2022-12-06 10:33:59 -0500

    Add files via upload

---
+++ c-sharp-minicapstonemodule1-team4/c-sharp-minicapstonemodule1-team4/capstone/Capstone/Inventory.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Capstone
+{
+    public class Inventory
+    {
+
+        public Dictionary<string, Snack> VendingInventory { get; set; }
+        public Inventory()
+        {
+            VendingInventory = new Dictionary<string, Snack>();
+        }
+
+        public void Populate()
+        {
+
+            //opens try catch for stream reader
+
+            try
+            {
+
+                // opens stream reader
+                List<string[]> chunks = new List<string[]>();
+                using(StreamReader sr = new StreamReader(@"C:\Users\Student\git\c-sharp-minicapstonemodule1-team4\capstone\vendingmachine.csv"))
+                {   
+                    //runs while not end of stream
+
+
+                    while(!sr.EndOfStream)
+                    {
+                        string[] snackTemp = sr.ReadLine().Split("|");
+                        decimal pr = Decimal.Parse(snackTemp[2]);
+                        if (snackTemp[3] == "Chip")
+                        {
+                            Chips snack = new Chips(snackTemp[0], snackTemp[1], pr, snackTemp[3]);
+                            VendingInventory.Add(snackTemp[0], snack);
+                        }
+                        if (snackTemp[3] == "Candy")
+                        {
+                            Candy snack = new Candy(snackTemp[0], snackTemp[1], pr, snackTemp[3]);
+                            VendingInventory.Add(snackTemp[0], snack);
+                        }
+                        if (snackTemp[3] == "Drink")
+                        {
+                            Drinks snack = new Drinks(snackTemp[0], snackTemp[1], pr, snackTemp[3]);
+                            VendingInventory.Add(snackTemp[0], snack);
+                        }
+                        if (snackTemp[3] == "Gum")
+                        {
+                            Gum snack = new Gum(snackTemp[0], snackTemp[1], pr, snackTemp[3]);
+                            VendingInventory.Add(snackTemp[0], snack);
+                        }
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+        public void ListItems()
+        {
+            foreach (Snack sn in VendingInventory.Values)
+            {
+                Console.WriteLine($"{sn.Slot} | {sn.SnackName}: ${sn.Price}");
+            }
+        }
+
+        public void DisplaySalesReport()
+        {
+            decimal totalSales = 0;
+
+            foreach(Snack sn in VendingInventory.Values)
+            {
+                Console.WriteLine(sn.SnackName + "|" + sn.TotalSold);
+                totalSales += (sn.TotalSold * sn.Price);  
+            }
+            Console.WriteLine($"**TOTAL SALES** {totalSales:C2}");
+        }
+
+        public decimal FeedMoney(decimal fedMoney)
+        {
+            bool success = false;
+            do
+            {
+                Console.WriteLine("Enter your money in whole dollar amounts please!");
+                string entry = Console.ReadLine();
+
+                success = Int32.TryParse(entry, out int result);
+
+                if (success == true)
+                {
+                    fedMoney += decimal.Parse(entry);
+                }
+            }
+            while (success == false);
+
+            return fedMoney;
+        }
+        public decimal Dispense(string input, decimal money)
+        {
+            VendingInventory[input].Quantity--;
+            VendingInventory[input].TotalSold++;
+            money = money - VendingInventory[input].Price;
+            VendingInventory[input].DispenseMessage();
+            return money;
+        }
+        public decimal Dispense(string input, decimal money, int quant)
+        {
+            VendingInventory[input].Quantity -= quant;
+            VendingInventory[input].TotalSold++;
+            money = money - VendingInventory[input].Price;
+            Console.Write("");
+            VendingInventory[input].DispenseMessage();
+            return money;
+        }
+        public void GetChange(decimal customerMoney)
+        {
+            decimal expanded = (customerMoney * 100);
+            decimal quarters = Math.Floor(expanded / 25);
+            decimal remaining = expanded % 25;
+            decimal dimes = Math.Floor(remaining / 10);
+            remaining = remaining % 10;
+            decimal nickels = Math.Floor(remaining / 5);
+            Console.WriteLine($"CHANGE RETURNED \nQuarters: {quarters}\nDimes: {dimes}\nNickels: {nickels}");
+        }
+
+        public void SelectProduct (string userSelect, decimal money, string path)
+        {
+            if (VendingInventory.ContainsKey(userSelect))
+            {
+                if (VendingInventory[userSelect].Quantity > 0)
+                {
+                    if (VendingInventory[userSelect].Price <= money)
+                    {
+                        money = Dispense(userSelect, money);
+                        File.AppendAllText(path, $"\n{DateTime.Today.Month}/{DateTime.Today.Day}/{DateTime.Today.Year}  {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second} {userSelect} {VendingInventory[userSelect].SnackName} {money:C2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Insufficient Funds");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sold Out");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Slot");
+            }
+        }
+        public void SelectProduct(string userSelect, decimal money, string path, int purchaseQuantity)
+        {
+            if (VendingInventory.ContainsKey(userSelect))
+            {
+                if (VendingInventory[userSelect].Quantity > 0)
+                {
+                    if(VendingInventory[userSelect].Quantity > purchaseQuantity)
+                    {
+                        if (VendingInventory[userSelect].Price <= money)
+                        {
+                            money = Dispense(userSelect, money, purchaseQuantity);
+                            File.AppendAllText(path, $"\n{DateTime.Today.Month}/{DateTime.Today.Day}/{DateTime.Today.Year}  {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second} {userSelect} {VendingInventory[userSelect].SnackName} {money:C2}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Insufficient Funds");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not enough product for selected quantity");
+                    }   
+                }
+                else
+                {
+                    Console.WriteLine("Sold Out");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Slot");
+            }
+        }
+    }
+}
---
+++ c-sharp-minicapstonemodule1-team4/c-sharp-minicapstonemodule1-team4/capstone/Capstone/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace Capstone
+{
+    public class Program
+
+    {       
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Student\git\c-sharp-minicapstonemodule1-team4";
+            string fileName = @"\Log.txt";
+            string fullPath = filePath + fileName;
+
+            Inventory inv = new Inventory();
+            inv.Populate();
+
+            bool isRunning = true;
+            bool transaction = false;
+            string log = "";
+
+
+            while(isRunning)
+            {
+                decimal userMoney = 0;
+                Console.WriteLine("(1) Display Vending Machine Items");
+                Console.WriteLine("(2) Purchase");
+                Console.WriteLine("(3) Exit");
+
+                string userInput = Console.ReadLine();
+
+                if (userInput.Equals("1"))
+                {
+                    inv.ListItems();
+                }
+                else if(userInput.Equals("2"))
+                {
+                    transaction = true;
+                    
+                    while(transaction == true)
+                    {
+                        Console.WriteLine($"Current Money Provided {userMoney:C2}");
+                        Console.WriteLine(" ");
+
+                        Console.WriteLine("(1) Feed Money");
+                        Console.WriteLine("(2) Select Product");
+                        Console.WriteLine("(3) Finish Transaction");
+
+                        userInput = Console.ReadLine();
+
+                        if (userInput.Equals("1"))
+                        {
+                            decimal temp = userMoney;
+                            userMoney = inv.FeedMoney(userMoney);
+                            File.AppendAllText(fullPath,$"\n{DateTime.Today.Month}/{DateTime.Today.Day}/{DateTime.Today.Year}  {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second} FEED MONEY: {temp:C2} {userMoney:C2}");
+                        }
+                        else if (userInput.Equals("2"))
+                        {
+                            Console.Write("Please Enter Slot:");
+                            string userSelectionInput = Console.ReadLine().ToUpper();
+                            if(userSelectionInput.Length == 2)
+                            {
+                                inv.SelectProduct(userSelectionInput, userMoney, fullPath);
+                            }
+                            if(Int32.TryParse(userSelectionInput.Substring(3,userSelectionInput.Length-3),out int result))
+                            {
+                                inv.SelectProduct(userSelectionInput, userMoney, fullPath, Int32.Parse(userSelectionInput.Substring(3, userSelectionInput.Length - 3)));
+                            }
+                            
+                        }
+                        else if (userInput.Equals("3"))
+                        {
+                            File.AppendAllText(fullPath, $"\n{DateTime.Today.Month}/{DateTime.Today.Day}/{DateTime.Today.Year}  {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second} GIVE CHANGE {userMoney:C2} $0.00");
+                            inv.GetChange(userMoney);
+                            transaction = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please select a number 1 through 3.");
+                        }
+                    }
+
+                   
+                }
+                else if(userInput.Equals("3"))
+                {
+                    isRunning = false;
+                    Console.WriteLine("Thank you for your purchase!");
+                }
+                else if(userInput.Equals("4"))
+                {
+                    inv.DisplaySalesReport();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input please enter a number 1-3");
+                }
+            }
+        }
+
+    }
+
+}
